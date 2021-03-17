2c5ac690cd081aa66c0795c46ef818e1c3765f86
Le Trashman <o12ri3@gmail.com>
2021-03-17 10:46:34 +0000

    Proper colours in skill descriptions

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 
 from jinja2 import Environment, FileSystemLoader
@@ -7,20 +8,29 @@ from data import load_data
 from model import Character
 
 
-def generate(path):
+def colorize(value):
+    return re.sub(
+        r'\[c]\[([0-9A-Fa-f]{6})]([^\[]*)\[-]\[/c]',
+        r'<span style="color: #\1">\2</span>',
+        value
+    )
+
+
+def generate(path, character_id):
     data = load_data(path)
-    character = Character.from_data(10000, data)
+    character = Character.from_data(character_id, data)
 
     env = Environment(loader=FileSystemLoader(os.path.dirname(__file__)))
+    env.filters['colorize'] = colorize
     template = env.get_template('template.txt')
     print(template.render(character=character))
 
 
 def main():
     try:
-        generate(sys.argv[1])
-    except IndexError:
-        print('usage: generate.py <path>')
+        generate(sys.argv[1], int(sys.argv[2]))
+    except (IndexError, ValueError):
+        print('usage: generate.py <path> <character id>')
 
 
 if __name__ == '__main__':
